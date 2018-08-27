76bc5dc9ef462d6103b8a0b74cc0bfda238e2a0e
erandro <erandro@gmail.com>
2018-08-26 22:30:23 -0500

    added the band search and omdb - still need to fix bug to make them work

--- liri.js
+++ liri.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 var Spotify = require('node-spotify-api');
 var omdb = require('omdb');
+var request = require("request");
 var keys = require("./keys.js");
+var moment = require('moment');
 
 var spotify = new Spotify(keys.spotify);
 var action = process.argv[2];
 
-// if (action === "concert-this") {
-//     concertThis();
-// };
+if (action === "concert-this") {
+    concertThis();
+};
 if (action === "spotify-this-song") {
     spotifyThisSong();
 };
@@ -16,9 +18,18 @@ if (action === "movie-this") {
     movieThis();
 };
 
-// function concertThis() {
-//     var preformerName = process.argv[3];
-// }
+function concertThis() {
+    var preformerName = process.argv[3];
+    request("https://rest.bandsintown.com/artists/" + preformerName + "/events?app_id=codingbootcamp", function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(JSON.parse(body));
+            //console.log("Venue: " + body.venue.name);
+            //console.log("City: " + body[0].venue);
+            //var eventDate = moment(body[0].datetime).format("MM-DD-YYYY");
+            //console.log("Date: " + eventDate);
+        };
+    });
+};
 function spotifyThisSong() {
     var songName = process.argv[3];
     if (songName === undefined) {
@@ -57,25 +68,17 @@ function spotifyThisSong() {
 };
 function movieThis() {
     var movieName = process.argv[3];
-    omdb.search(movieName, function (err, movies) {
-        if (err) {
-            return console.error(err);
-        } else {
-            console.log(movies);
-        }
-        // if (movies.length < 1) {
-        //     return console.log('No movies were found!');
-        // }
-        // movies.forEach(function (movie) {
-        //     console.log(movie.title, movie.year);
-        // });
+    request("http://www.omdbapi.com/?t=" + movieName + "&apikey=trilogy", function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            //console.log(JSON.parse(body));
+            console.log("Title: " + body.Title);
+            console.log("Year: " + body.Year);
+            console.log("Rated " + body.Rated);
+            console.log("Rotten Tomatoes score: " + body.Ratings);
+            console.log("Produced in: " + body.Country);
+            console.log("Language: " + body.Language);
+            console.log("Plot: " + body.Plot);
+            console.log("Actors: " + body.Actors);
+        };
     });
-    // Title of the movie.
-    // Year the movie came out.
-    // IMDB Rating of the movie.
-    // Rotten Tomatoes Rating of the movie.
-    // Country where the movie was produced.
-    // Language of the movie.
-    // Plot of the movie.
-    // Actors in the movie.
-}
+};
